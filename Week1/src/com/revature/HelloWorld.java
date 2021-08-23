47afa2330fa91dd2a2640846f45843cf354aaafc
ivonsurf123 <iaspilcueta74@gmail.com>
2021-08-23 16:04:35 -0600

    add HelloWorld class

---
+++ Week1/src/com/revature/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.revature;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		/**
+		 * this is a multi-line comment
+		 */
+		// syso +ctrl+space
+		
+		System.out.println("Hello World....from the IDE!");
+
+	}
+
+}
